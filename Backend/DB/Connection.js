21ba48302496ad8edb72f87806aa53cf43ac32f3
Hazem-Samir <hazemyugi010@gmail.com>
2024-04-16 02:41:14 +0200

    Making Docker Compose

--- Backend/DB/Connection.js
+++ Backend/DB/Connection.js
@@ -1,14 +1,20 @@
 
 // ====================== Initialization ======================
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'edara'   
-});
+const mysql = require('mysql2');
 
+
+
+
+process.env.MYSQL_DB
+const connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST|| '172.23.0.1"',
+    user     : process.env.MYSQL_USER||'root',
+    password : process.env.MYSQL_PASS || 'root',
+    database : process.env.MYSQL_DB||'edara',
+    port: process.env.MYSQL_PORT || '3306'
+})
 // ====================== Connect To SQL Server ======================
+setTimeout(()=>{
 connection.connect(function (err) {
     if (err) {
         console.error('error connecting: ' + err.stack);
@@ -16,6 +22,6 @@ connection.connect(function (err) {
     }
 
     console.log('connected as id ' + connection.threadId);
-});
+});},14000)
 
 module.exports = connection;
